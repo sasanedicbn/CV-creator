36c403237245d6963ce2e760e223e8e455e30778
Sasa Nedic <svetasmak0@gmail.com>
2024-04-03 20:20:25 +0200

    Create style for header component

--- CV-creator/src/App.tsx
+++ CV-creator/src/App.tsx
@@ -1,11 +1,14 @@
 import PersonalInformation from './components/PersonalInformation'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   return (
-    <>
+
+    <div className='main-container'>
+    <Header/>
     <PersonalInformation/>
-    </>
+    </div>
   )
 }
 
---
+++ CV-creator/src/components/Header.tsx
@@ -0,0 +1,6 @@
+const Header = () => {
+    return(
+        <h2 className="header">CV CREATOR</h2>
+    )
+}
+export default Header;
