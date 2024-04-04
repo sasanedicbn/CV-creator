db0523f7b35beca136999a9d3734fcbc06bbfdf2
Sasa Nedic <svetasmak0@gmail.com>
2024-04-04 19:56:03 +0200

    Create changes

--- CV-creator/src/components/Education.tsx
+++ CV-creator/src/components/Education.tsx
@@ -24,8 +24,8 @@ const Education = () => {
   function deleteExperience(index) {
    setEducation(education.filter((_,i) => i !== index))
   }
-
   
+ 
   return (
     <div className="container">
      {education.map((education, index) => (
@@ -35,27 +35,27 @@ const Education = () => {
          <div className="education-item">
            <div className="form-group">
              <label htmlFor="university_name">University Name:</label>
-             <input type="text" id="university_name" name="university_name"   className="form-input" />
+             <input type="text" id="university_name" name="university_name"  onChange={e => handleInputData(e, index, 'universityName' )} className="form-input" />
            </div>
            <div className="form-group">
              <label htmlFor="city">City:</label>
-             <input type="text" id="city" name="city" className="form-input" />
+             <input type="text" id="city" name="city" onChange={e => handleInputData(e, index, 'city' )} className="form-input" />
            </div>
            <div className="form-group">
              <label htmlFor="degree">Degree:</label>
-             <input type="text" id="degree" name="degree" className="form-input" />
+             <input type="text" id="degree" onChange={e => handleInputData(e, index, 'degree' )} name="degree" className="form-input" />
            </div>
            <div className="form-group">
              <label htmlFor="subject">Subject:</label>
-             <input type="text" id="subject" name="subject" className="form-input" />
+             <input type="text" id="subject" name="subject" onChange={e => handleInputData(e, index, 'subject' )} className="form-input" />
            </div>
            <div className="form-group">
              <label htmlFor="from_date">From (dd/mm/yyyy):</label>
-             <input type="text" id="from_date" name="from_date" className="form-input" />
+             <input type="text" id="from_date" name="from_date" onChange={e => handleInputData(e, index, 'from' )} className="form-input" />
            </div>
            <div className="form-group">
              <label htmlFor="to_date">To (dd/mm/yyyy):</label>
-             <input type="text" id="to_date" name="to_date" className="form-input" />
+             <input type="text" id="to_date" onChange={e => handleInputData(e, index, 'to' )} name="to_date" className="form-input" />
            </div>
           {index === 0 ? null : <button className="delete-button" onClick={() => deleteExperience(index)} >Delete</button>}
          </div>
