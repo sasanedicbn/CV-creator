59f88e5d535fc2561456fff108fc8d9ed4d28682
Sasa Nedic <svetasmak0@gmail.com>
2024-04-05 20:05:43 +0200

    Add print funcitonality

--- CV-creator/src/components/CVAccount.tsx
+++ CV-creator/src/components/CVAccount.tsx
@@ -1,16 +1,22 @@
+import { useRef } from 'react';
+import { useReactToPrint } from 'react-to-print';
 const CVAccount = ({dataForAccount, removeAccountComponent}) => {
+    const printRef = useRef()
+    const handlePrint = useReactToPrint({
+        content: () => printRef.current,
+      });
     const {educationData, experienceData, personalInfo } = dataForAccount
     console.log(dataForAccount)
     return(
-        <>
+        <div ref={printRef}>
         <div className="overlay"></div>
         <div className="cv-account">
+        <p className="profile-img"><strong>Photo:</strong> <img src={personalInfo?.photo} alt="Profile" /></p>
         <div className="personal-info">
           <h2>Personal Information</h2>
           <p><strong>First Name:</strong> {personalInfo?.firstName}</p>
           <p><strong>Last Name:</strong> {personalInfo?.lastName}</p>
           <p><strong>Title:</strong> {personalInfo?.title}</p>
-          <p><strong>Photo:</strong> <img src={personalInfo?.photo} alt="Profile" /></p>
           <p><strong>Address:</strong> {personalInfo?.address}</p>
           <p><strong>Phone Number:</strong> {personalInfo?.phoneNumber}</p>
           <p><strong>Email:</strong> {personalInfo?.email}</p>
@@ -45,10 +51,10 @@ const CVAccount = ({dataForAccount, removeAccountComponent}) => {
         </div>
         <div className="btn-container">
             <button className="custom-btn" onClick={removeAccountComponent}> Go back </button>
-            <button className="custom-btn"> Print </button>
+            <button className="custom-btn" onClick={handlePrint}> Print </button>
         </div>
       </div>
-      </>
+      </div>
     )
 }
 export default CVAccount;
--- CV-creator/src/components/Preview.tsx
+++ CV-creator/src/components/Preview.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState  } from "react";
 import CVAccount from "./CVAccount";
+
 
 const Preview = ({experienceData, educationData, personalnfo}) => {
     const [showAccount, setShowAccount] = useState(false)
