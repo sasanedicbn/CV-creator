f1bf2c334b2ce04b85a4240360395718a208b30b
Sasa Nedic <svetasmak0@gmail.com>
2024-04-05 14:42:51 +0200

    Create CVAccount component

--- CV-creator/src/components/CVAccount.tsx
+++ CV-creator/src/components/CVAccount.tsx
@@ -1,7 +1,47 @@
 const CVAccount = ({dataForAccount}) => {
+    const {educationData, experienceData, personalInfo } = dataForAccount
     console.log(dataForAccount)
     return(
-     <p>SAas</p>
+        <div className="cv-account">
+        <div className="personal-info">
+          <h2>Personal Information</h2>
+          <p><strong>First Name:</strong> {personalInfo?.firstName}</p>
+          <p><strong>Last Name:</strong> {personalInfo?.lastName}</p>
+          <p><strong>Title:</strong> {personalInfo?.title}</p>
+          <p><strong>Photo:</strong> <img src={personalInfo?.photo} alt="Profile" /></p>
+          <p><strong>Address:</strong> {personalInfo?.address}</p>
+          <p><strong>Phone Number:</strong> {personalInfo?.phoneNumber}</p>
+          <p><strong>Email:</strong> {personalInfo?.email}</p>
+          <p><strong>Description:</strong> {personalInfo?.description}</p>
+        </div>
+        
+        <div className="education-info">
+          <h2>Education</h2>
+          {educationData.map((edu, index) => (
+            <div key={index}>
+              <p><strong>University Name:</strong> {edu.universityName}</p>
+              <p><strong>City:</strong> {edu.city}</p>
+              <p><strong>Degree:</strong> {edu.degree}</p>
+              <p><strong>Subject:</strong> {edu.subject}</p>
+              <p><strong>From:</strong> {edu.from}</p>
+              <p><strong>To:</strong> {edu.to}</p>
+            </div>
+          ))}
+        </div>
+  
+        <div className="experience-info">
+          <h2>Experience</h2>
+          {experienceData.map((exp, index) => (
+            <div key={index}>
+              <p><strong>Position:</strong> {exp.position}</p>
+              <p><strong>Company:</strong> {exp.company}</p>
+              <p><strong>City:</strong> {exp.city}</p>
+              <p><strong>From:</strong> {exp.from}</p>
+              <p><strong>To:</strong> {exp.to}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     )
 }
 export default CVAccount;
