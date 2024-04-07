423e593f4541234f15bf70030645df14bb71e5ad
Sasa Nedic <svetasmak0@gmail.com>
2024-04-07 13:30:43 +0200

    code reorganization

--- CV-creator/src/App.tsx
+++ CV-creator/src/App.tsx
@@ -8,6 +8,21 @@ import Wrapper from './components/Wrapper'
 import { useState } from 'react'
 import { EducationData, ExperienceData, PersonalInfo } from './components/TypeScript'
 function App() {
+const [CV, setCV] = useState({
+  personalInformation:{
+    firstName: '',
+    lastName: '',
+    title: '',
+    photo: '',
+    address: '',
+    phoneNumber: '',
+    email: '',
+    description: '',
+  },
+  educationData: [{}],
+  experience: [{}],
+})
+{'ovvoo je',console.log(CV)}
 const [educationData, setEducationData] = useState<EducationData[]>([])
 const [experienceData, setExperienceData] = useState<ExperienceData[]>([])
 const [personalnfo, setPersonalInfo] = useState<PersonalInfo>({
@@ -34,7 +49,7 @@ const handlePersonInfo = (data: PersonalInfo) => {
   return (
     <Wrapper>
     <Header/>
-    <PersonalInformation handlePersonInfo={handlePersonInfo}/>
+    <PersonalInformation handlePersonInfo={handlePersonInfo} setCV={setCV}/>
     <Education handleEducationData={handleEducationData}/>
     <Experience handleExperienceData={handleExperienceData}/>
     <Preview  educationData={educationData} experienceData={experienceData} personalnfo={personalnfo} />
--- CV-creator/src/components/PersonalInformation.tsx
+++ CV-creator/src/components/PersonalInformation.tsx
@@ -1,33 +1,19 @@
-import {  useEffect, useState } from "react";
 import { PersonalInfo } from "./TypeScript";
 type personalInfoProp = {
     handlePersonInfo: (data:PersonalInfo) => void;
 }
 
 
-const PersonalInformation = ({handlePersonInfo}:personalInfoProp) => {
-    const [personalInfo, setPersonalInfo] = useState({
-        firstName: '',
-        lastName: '',
-        title: '',
-        photo: '',
-        address: '',
-        phoneNumber: '',
-        email: '',
-        description: '',
-    })
+const PersonalInformation = ({handlePersonInfo, setCV}:personalInfoProp) => {
 
-    function handleInputData  (event: React.ChangeEvent<HTMLInputElement>, filed: keyof PersonalInfo){
+    function handleInputData  (event: React.ChangeEvent<HTMLInputElement>){
         const value = event.target.value;
+        const name = event.target.name
         // za sliku ovo dole ne citam sliku sa event.trget.value
-        {console.log(URL.createObjectURL(event.target.files[0]))}
+        // {console.log(URL.createObjectURL(event.target.files[0]))}
 
-         setPersonalInfo(prevState => ({...prevState,
-            [filed]: value}))
+        setCV(prevCV => ({...prevCV, personalInformation: {...prevCV.personalInformation, [name]: value}}));
     }
-    useEffect(() => {
-        handlePersonInfo(personalInfo)
-    }, [personalInfo])
     
     // napraviti child komponentu za form-group da bude citljivije
 
@@ -36,35 +22,35 @@ const PersonalInformation = ({handlePersonInfo}:personalInfoProp) => {
       <h2 className="form-title">Personal Information</h2>
       <div className="form-group">
         <label htmlFor="first_name">First Name:</label>
-        <input type="text" id="first_name" name="first_name" className="form-input" onChange={(e) => handleInputData(e, 'firstName')}/>
+        <input type="text" id="first_name" name="first_name" className="form-input" onChange={(e) => handleInputData(e)}/>
       </div>
       <div className="form-group">
         <label htmlFor="last_name">Last Name:</label>
-        <input type="text" id="last_name" name="last_name" className="form-input"  onChange={(e) => handleInputData(e, 'lastName')}/>
+        <input type="text" id="last_name" name="last_name" className="form-input"  onChange={(e) => handleInputData(e)}/>
       </div>
       <div className="form-group">
         <label htmlFor="title">Title:</label>
-        <input type="text" id="title" name="title" className="form-input"  onChange={(e) => handleInputData(e, 'title')}/>
+        <input type="text" id="title" name="title" className="form-input"  onChange={(e) => handleInputData(e)}/>
       </div>
       <div className="form-group">
         <label htmlFor="photo">Photo:</label>
-        <input type="file" id="photo" name="photo" className="form-input" onChange={(e) => handleInputData(e,  'photo')} />
+        <input type="file" id="photo" name="photo" className="form-input" onChange={(e) => handleInputData(e)} />
       </div>
       <div className="form-group">
         <label htmlFor="address">Address:</label>
-        <input type="text" id="address" name="address" className="form-input" onChange={(e) => handleInputData(e,  'address')} />
+        <input type="text" id="address" name="address" className="form-input" onChange={(e) => handleInputData(e)} />
       </div>
       <div className="form-group">
         <label htmlFor="phone">Phone Number:</label>
-        <input type="tel" id="phone" name="phone" className="form-input" onChange={(e) => handleInputData(e,  'phoneNumber')}/>
+        <input type="tel" id="phone" name="phone" className="form-input" onChange={(e) => handleInputData(e)}/>
       </div>
       <div className="form-group">
         <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" className="form-input" onChange={(e) => handleInputData(e,  'email')} />
+        <input type="email" id="email" name="email" className="form-input" onChange={(e) => handleInputData(e)} />
       </div>
       <div className="form-group">
         <label htmlFor="description">Description:</label>
-        <input type="text" id="title" name="title" className="form-input" onChange={(e) => handleInputData(e,  'description')}/>
+        <input type="text" id="title" name="title" className="form-input" onChange={(e) => handleInputData(e)}/>
       </div>
     </div>
   );
